622630c131ce54df0218fc67d7ce4574a325357e
henrikvilhelmberglund <henrikvilhelmberglund@gmail.com>
2023-04-21 00:49:04 +0200

    feat: :wrench: add mdsvex config

---
+++ mdsvex.config.js
@@ -0,0 +1,26 @@
+import { defineMDSveXConfig as defineConfig } from "mdsvex";
+import examples from "mdsvexamples";
+
+const config = defineConfig({
+	extensions: [".svelte.md", ".md", ".svx"],
+
+	smartypants: {
+		dashes: "oldschool",
+		quotes: false,
+	},
+	layout: {
+		notes: "src/lib/mdsvex-layout-notes.svx",
+	},
+	remarkPlugins: [
+		examples,
+		{
+			defaults: {
+				Wrapper: "/src/lib/Example.svelte",
+			},
+		},
+	],
+
+	rehypePlugins: [],
+});
+
+export default config;
