0376777b1fd34d1424108f5d18d9ae9835e50edc
henrikvilhelmberglund <henrikvilhelmberglund@gmail.com>
2023-06-14 14:36:01 +0200

    feat: :sparkles: initialize Reveal.js (with hash for better HMR), render the slides in an each block

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -1,11 +1,30 @@
 <script>
 	import { base } from "$app/paths";
 	import Footer from "$lib/Footer.svelte";
+	import Reveal from "reveal.js";
+	import { onMount } from "svelte";
+
+	export let data;
+
+	onMount(() => {
+		let deck = new Reveal({ hash: true });
+		deck.initialize();
+	});
 </script>
 
-<main class="[&>*]:m-4">
-  <p>Hi</p>
-</main>
+<div class="reveal">
+	<div class="slides">
+		{#each data.slides as slide}
+			<section>
+				{#each slide as element}
+					{@html element}
+				{/each}
+			</section>
+		{/each}
+	</div>
+</div>
+
+{@html `<` + `style>${data.css}</style>`}
 
 <!-- TODO fix so you can see when no scroll -->
 <Footer />
