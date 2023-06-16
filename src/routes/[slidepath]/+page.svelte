0536c6c397d7f38bebe4c8d4674da023bc3ceb4b
henrikvilhelmberglund <henrikvilhelmberglund@gmail.com>
2023-06-16 16:30:57 +0200

    feat: :sparkles: add HMR via vite to refresh slides when edited
    past me was a genius

--- src/routes/[slidepath]/+page.svelte
+++ src/routes/[slidepath]/+page.svelte
@@ -2,20 +2,35 @@
 	import { base } from "$app/paths";
 	import Footer from "$lib/Footer.svelte";
 	import Reveal from "reveal.js";
-	import { onMount } from "svelte";
+	import { onMount, afterUpdate, beforeUpdate } from "svelte";
+  import { tick } from "svelte";
+
+  if (import.meta.hot) {
+    import.meta.hot.on("vite:beforeUpdate", () => {
+      // console.log("Running before update!!");
+    });
+    import.meta.hot.on("vite:afterUpdate", () => {
+      // console.log("Running after update!!");
+      // https://github.com/hakimel/reveal.js/issues/1177#issuecomment-86051667
+      deck.slide(deck.getIndices().h, deck.getIndices().v);
+    });
+  }
 
 	export let data;
+	let deck;
 
 	onMount(() => {
-		let deck = new Reveal({ hash: true, navigationMode: "linear" });
+		deck = new Reveal({ hash: true, navigationMode: "linear" });
 		deck.initialize();
 	});
+
+	$: slide = data.slide;
 </script>
 
 <div class="reveal">
 	<div class="slides">
 		<!-- <Slides /> -->
-    <svelte:component this={data.slide}/>
+		<svelte:component this={slide} />
 	</div>
 </div>
 
